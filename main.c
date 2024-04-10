c2800befe07d6a709d9c0cef7dd0b2b917fd6a32
Jimnora <djimnez@uc.cl>
2024-04-10 18:17:02 -0400

    fix: add newline to c prints

--- main.c
+++ main.c
@@ -6,19 +6,19 @@
 // Prime factorization based on: https://www.geeksforgeeks.org/print-all-prime-factors-of-a-given-number/
 void basicPrimeFactorization(long long n) {
     while (n % 2 == 0) {
-        printf("%d ", 2);
+        printf("%d \n", 2);
         n = n / 2;
     }
 
     for (int i = 3; i * i <= n; i = i + 2) {
         while (n % i == 0) {
-            printf("%d ", i);
+            printf("%d \n", i);
             n = n / i;
         }
     }
 
     if (n > 2) {
-        printf("%lld ", n);
+        printf("%lld \n", n);
     }
 }
 
