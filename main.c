db192ed19d57c066ce2a18e91ce0d9aeaa24573e
donjavi-538 <javier.v.armijo@uc.cl>
2024-04-11 19:08:16 -0400

    Update: saving numbers in an array

--- main.c
+++ main.c
@@ -3,23 +3,32 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define MAX_FACTORS 60
+
 // Prime factorization based on: https://www.geeksforgeeks.org/print-all-prime-factors-of-a-given-number/
-void basicPrimeFactorization(long long n) {
+int basicPrimeFactorization(long long n, int* factors) {
+    int count = 0;
+
     while (n % 2 == 0) {
-        printf("%d \n", 2);
+        factors[count] = 2;
+        count++;
         n = n / 2;
     }
 
     for (int i = 3; i * i <= n; i = i + 2) {
         while (n % i == 0) {
-            printf("%d \n", i);
+            factors[count] = i;
+            count++;
             n = n / i;
         }
     }
 
     if (n > 2) {
-        printf("%lld \n", n);
+        factors[count] = n;
+        count++;
     }
+
+    return count;
 }
 
 int main(int argc, char** argv) {
@@ -28,6 +37,14 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    basicPrimeFactorization(atoll(argv[1]));
+    long long num = atoll(argv[1]);
+    int factors[MAX_FACTORS];
+
+    int count = basicPrimeFactorization(num, factors);
+    printf("Cantidad de factores primos: %d \n", count);
+    printf("Factores: \n");
+    for (int i = 0; i < count; i++) {
+        printf("%d \n", factors[i]);
+    }
     return 0;
 }
