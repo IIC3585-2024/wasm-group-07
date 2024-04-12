5d54eabcaa48fec63ea456741029160b0926358c
Jimnora <djimnez@uc.cl>
2024-04-11 22:12:43 -0400

    feat: allow Bigints to be passed to wasm

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-const { basicFactorization } = require('./basicFactorization');
+function generateArrayAndPrint() {
+    // Get the input value and split it into an array of numbers
+    const number = BigInt(document.getElementById('numberInput').value);
+    console.log(`[JS] Number: ${number}`);
 
-n = process.argv[2];
+    // Create a BigInt64Array with a size of 60
+    const bigint64Array = new BigInt64Array(60);
 
-factors = basicFactorization(n);
-console.log(factors.join(' '));
+    // Allocate memory for the BigInt64Array in the WebAssembly module
+    const pointer = Module._malloc(bigint64Array.length * bigint64Array.BYTES_PER_ELEMENT);
+
+    // Separate the memory address of the BigInt64Array into 32-bit integers
+    const pointer32 = pointer / 4;
+    const pointerHigh = pointer32 + 1;
+
+    // Set the memory address of the BigInt64Array in the WebAssembly module
+    Module.HEAP32[pointer32] = pointer;
+    Module.HEAP32[pointerHigh] = 0;
+
+    // Call the prime_factorization function from the WebAssembly module
+    const _prime_factorization = Module.cwrap('PrimeFactorization', "number", ['bigint', "[bigint]"]);
+
+    // Call the prime_factorization function from the WebAssembly module
+    const counter = _prime_factorization(number, pointer);
+
+    console.log(`[JS] Called prime_factorization`);
+
+   // from pointer to BigInt64Array
+    const resultArray = new BigInt64Array(Module.HEAP32.buffer, pointer, counter);
+
+    
+
+    // Display the result in the HTML
+    const resultDiv = document.getElementById('printArrayResult');
+
+    const resultArrayString = Array.from(resultArray).join(', ');
+
+    resultDiv.textContent = `The number ${number} has ${counter} factors: ${resultArrayString}`;
+
+    // resultDiv.textContent = `The number ${number} has ${counter} factors.`;
+    // // Free the allocated memory
+    Module._free(pointer);
+}
